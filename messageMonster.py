b84a440658a47dc8182ac6c017622f4d1f683646
Andy <ndmaque@gmail.com>
2023-11-27 14:08:47 +0000

    code tidy and comment updates

--- messageMonster.py
+++ messageMonster.py
@@ -22,7 +22,7 @@
 # puts them in a node_modules folder and we have a .gitignore entry for this folder with thousands of files
 
 # $ npm install
-# only above once, np if you do tho
+# only run above once, np if you do tho
 # now the packages are in place run the app
 # $ node api.js
 
@@ -39,18 +39,8 @@
 from datetime import datetime
 import time
 import shutil
-from shutil import copytree, ignore_patterns
-
 import tools # imports tools.py with our custom functions to save clutter
-import glob
 
-# absolute path to search all text files inside a specific folder
-path = r'inbox_examples/*.txt'
-files = glob.glob(path)
-print(files[0])
-
-
-# node should be running the server in folder api/api.js
 api_url = 'http://localhost:3000/api/'
 
 # fetch and populate global variables from the http api
@@ -59,16 +49,15 @@ messages = tools.get_api_data(api_url +'messages')
 messages_sent = tools.get_api_data(api_url + 'messages_sent')
 
 
-
 def main():
     print('Api found: {} Users, {} Messages, {} Messages_Sent'.format(len(users), len(messages), len(messages_sent)))
     # Load dummy data: copy the /inbox_examples files to /inbox so we have something to process
-    shutil.copytree('inbox_examples', 'inbox', ignore=ignore_patterns('\.git*'), dirs_exist_ok=True)
+    shutil.copytree('inbox_examples', 'inbox', ignore=shutil.ignore_patterns('\.git*'), dirs_exist_ok=True)
 
     # loop forever checking for new files to read and post to the api storage 
     while True:
         tools.process_inbox_messages(messages, api_url)
-        time.sleep(1)
+        time.sleep(1) # snooze
 
 main()
 
@@ -76,10 +65,9 @@ main()
 # data should not be hard coded in the script but stored externally
 # and most of our 'helper' functions don't need to be in here either, just the core code
 # json is a common language that everyone speaks
-# we saved our data and left it open for other systems to read and write without python skills
-# how to use pip to install other libraries or packages
+# we saved our data externally for other systems to read and write
 
-# NB: the add_message() method could also go into tools but will need a tweak
-# Tip: open a terminal and tail the json files to see changes
-#      tail -f files/messages.json
+
+# Tip: open a terminal and tail the messages.json files to see changes
+# $ tail -f data_files/messages.json
 
